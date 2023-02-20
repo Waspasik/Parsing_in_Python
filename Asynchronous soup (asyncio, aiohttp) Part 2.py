28d1399f385055e50dec5fa083295c665c27f7df
Maksim Kisel <103454376+Waspasik@users.noreply.github.com>
2023-02-20 09:00:47 +0300

    Delete funcs get_data, get_soup, сhange func main

--- Asynchronous soup (asyncio, aiohttp) Part 2.py
+++ Asynchronous soup (asyncio, aiohttp) Part 2.py
@@ -9,51 +9,31 @@ import aiohttp
 import asyncio
 import requests
 from bs4 import BeautifulSoup
-from aiohttp_retry import RetryClient, ExponentialRetry
 
 
-links = []
-total = 0
-domain = 'https://parsinger.ru/asyncio/create_soup/1/'
-
-
-def get_soup(url):
+def get_urls():
     response = requests.get(url=url)
-    return BeautifulSoup(response.text, 'lxml')
-
-
-def get_urls(soup):
+    soup = BeautifulSoup(response.text, 'lxml')
+    shema = 'https://parsinger.ru/asyncio/create_soup/1/'
     all_links = soup.find('div', class_='item_card').find_all('a')
     for link in all_links:
-        links.append(domain + link['href'])
+        links.append(shema + link['href'])
 
 
-async def get_data(session, link):
-    retry_options = ExponentialRetry(attempts=5)
-    retry_client = RetryClient(raise_for_status=False, retry_options=retry_options, client_session=session,
-                               start_timeout=0.5)
-    async with retry_client.get(link) as response:
-        #print(response.status)
-        if response.ok:
-            resp = await response.text()
-            soup = BeautifulSoup(resp, 'lxml')
-            global total
-            total += int(soup.find('p', class_='text').text)
-
-
-async def main():
+async def main(link):
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        for link in links:
-            task = asyncio.create_task(get_data(session, link))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+        async with session.get(link) as response:
+            if response.ok:
+                resp = await response.text()
+                soup = BeautifulSoup(resp, 'lxml')
+                total.append(int(soup.find('p', class_='text').text))
 
 
 url = 'https://parsinger.ru/asyncio/create_soup/1/index.html'
-soup = get_soup(url)
-get_urls(soup)
+links, total = [], []
+get_urls()
 
+task = [main(link) for link in links]
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncio.run(main())
-print(total)
+asyncio.run(asyncio.wait(task))
+print(sum(total))
